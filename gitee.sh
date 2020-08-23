dae402d2b57c6de0c37e3ebabbf8a4238a369414
小东 <49069586+x-dr@users.noreply.github.com>
2020-08-23 17:33:10 +0800

    Update gitee.sh

--- gitee.sh
+++ gitee.sh
@@ -2,17 +2,16 @@
 
 set -e
 
-SOURCE_REPO=$1
-DESTINATION_REPO=$2
+GITHUB_REPO=$1
+GITEE_REPO=$2
 
 GIT_SSH_COMMAND="ssh -v"
 
-echo "SOURCE=$SOURCE_REPO"
-echo "DESTINATION=$DESTINATION_REPO"
+echo "GITHUB=$GITHUB_REPO"
+echo "GITEE=$GITEE_REPO"
 
-git clone --mirror "$SOURCE_REPO" && cd `basename "$SOURCE_REPO"`
-git remote set-url --push origin "$DESTINATION_REPO"
+git clone --mirror "$GITHUB_REPO" && cd `basename "$GITHUB_REPO"`
+git remote set-url --push origin "$GITEE_REPO"
 git fetch -p origin
-# Exclude refs created by GitHub for pull request.
 git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
 git push --mirror
